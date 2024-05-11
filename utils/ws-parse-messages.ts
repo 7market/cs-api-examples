044eab060ef08a96425fe6f21ccff272c52d0352
dev7market <dev@cs7.market>
2024-05-11 21:27:37 +0500

    feat: websocket message json replaced to object

--- utils/ws-parse-messages.ts
+++ utils/ws-parse-messages.ts
@@ -2,11 +2,5 @@ import { PublicationContext } from 'centrifuge';
 import { I7MarketWsMessages } from '../generated-api/cs7market-api';
 
 export const wsParseMessage = (message: PublicationContext) => {
-  try {
-    const parsedData = JSON.parse(message.data as string) as I7MarketWsMessages;
-
-    return parsedData;
-  } catch (e) {
-    console.error(e);
-  }
+  return message.data as I7MarketWsMessages;
 };
